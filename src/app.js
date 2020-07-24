e4e08a6f29a8a488cc6f07be6a4e5ea7df8651f0
Rodrigo Nogueira <nogueirajsx@gmail.com>
2020-07-24 15:41:14 -0300

    work

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+import React, { Component } from 'react'
+import MarkdownEditor from './components/markdown-editor'
+import marked from 'marked'
+
+import 'normalize.css'
+import './css/style.css'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {value: ''}
+    this.handleChange = (e) => {
+      this.setState({value: e.target.value})
+    }
+
+    this.getMarkup =()=> {
+      return {__html: marked(this.state.value)}
+    }
+  }
+
+  render () {
+    console.log(this.state.value)
+    return (
+      <MarkdownEditor 
+        value={this.state.value} 
+        handleChange={this.handleChange} 
+        getMarkup={this.getMarkup}
+      />
+    )
+  }
+}
+
+export default App
---
+++ src/components/markdown-editor.js
@@ -0,0 +1,15 @@
+import React, {PropTypes} from 'react'
+
+const MarkdownEditor = ({value, handleChange, getMarkup}) => (
+    <div className='editor'>
+      <textarea name='textarea' value={value} onChange={handleChange} autoFocus />
+        <div className='view' dangerouslySetInnerHTML={getMarkup()} />
+    </div>
+)
+
+MarkdownEditor.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
+}
+
+export default MarkdownEditor
